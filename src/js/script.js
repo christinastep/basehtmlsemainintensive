f9b7b7fcb075b65bd5d6b9708d519353853d5584
christinastep <christina.stephan@hetic.net>
2019-05-28 13:33:12 +0200

    update profil

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 document.querySelector(".button--nav").addEventListener("click", function(){
   document.querySelector(".sidebar").classList.toggle("active");
-})
+});
+
+document.querySelector(".button--newPost").addEventListener("click", function(){
+  document.querySelector(".post--new").classList.toggle("active");
+});
